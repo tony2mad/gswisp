95685d1237dbc6c77a833db261eef1868774fccf
tony2mad <meridian.arts2@gmail.com>
2024-12-14 21:10:51 +0100

    Fix SPA routing for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gswisp-website/', // Add this line for GitHub Pages
+  base: '/',
   build: {
     target: 'esnext',
     minify: 'esbuild',
