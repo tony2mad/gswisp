57748085f5680a92636962d6937e3ace2bb6ee0c
tony2mad <meridian.arts2@gmail.com>
2024-12-14 19:32:58 +0100

    Fix GitHub Pages configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/gswisp-website/', // Add this line for GitHub Pages
   build: {
     target: 'esnext',
     minify: 'esbuild',
